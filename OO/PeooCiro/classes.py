579531f21b8b741129ff5f56f4c81d5e34d0dc33
GERSON EMANUEL SALES DOS SANTOS <20231094010008@ifrn.local>
2024-10-10 11:36:47 -0300

    peoo

---
+++ OO/PeooCiro/classes.py
@@ -0,0 +1,10 @@
+class Animal:
+    nome = ''
+    peso=0
+
+    def imprimir(self):
+        print(f'meu nome: {self.nome}')
+        print(f'meu peso: {self.peso}')
+
+    def alimentar(self):
+        self.peso += comida
