10d899a129dcb5a70ed8c1c80a3a1a7537b6a951
GERSON EMANUEL SALES DOS SANTOS <20231094010008@ifrn.local>
2024-10-24 11:02:04 -0300

    vlah

--- OO/PeooCiro/alunos.py
+++ OO/PeooCiro/alunos.py
@@ -46,6 +46,16 @@ class Pessoa:
         self.idade +=1 
         
 
+class Alunos:
+    alunos_na_escola = []
+    def adicionar_aluno(self, aluno):
+        self.alunos_na_escola.append(aluno)
+
+    def mostrar_alunos(self):
+        for i in self.alunos_na_escola:
+            print(i)
+
+            
 class Lampada:
     def __init__(self, acesa:bool, volts:float) -> None:
         self.acesa = False
