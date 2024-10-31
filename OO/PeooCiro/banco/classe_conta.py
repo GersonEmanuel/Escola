780632702ef5ae89152603cfe2fda9998368fbf0
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-31 18:10:14 -0300

    get user blah

--- OO/PeooCiro/banco/classe_conta.py
+++ OO/PeooCiro/banco/classe_conta.py
@@ -12,6 +12,9 @@ class Usuario:
         self.cpf = cpf
         self.nascimento = nascimento
 
+    def __str__(self):
+        return f'nome: {self.nome}, cpf: {self.cpf}, ano de nascimento: {self.nascimento}'
+
 class BancoManagement:
     banco_contas = []
 
@@ -19,8 +22,8 @@ class BancoManagement:
         for i in range(len(BancoManagement.banco_contas)):
             print(BancoManagement.banco_contas[i]) 
 
-    def create_conta(self, usercpf:str):
-        if UsuariosManagement.user_existe(self, usercpf):
+    def create_conta(self, cpf):
+        if UsuariosManagement.user_existe(self, usercpf=cpf):
             conta_user = Conta(numero_conta=random.randint(1000,10000), agencia=str(input('Em qual agencia deseja criar? ')), dono= user, saldo=0)
             if BancoManagement.conta_existe(conta_user):
                 print('Não é possivel adicionar essa conta, ela ja existe no banco')
@@ -41,20 +44,28 @@ class UsuariosManagement:
     users = []
 
     def show_users(self):
-        for i in range(len(UsuariosManagement.users)):
-            print(UsuariosManagement.users[i])
+        for i in UsuariosManagement.users:
+            print(i)
+
 
     def user_existe(self, usercpf):
         for users in UsuariosManagement.users:
             if users.cpf == usercpf:
                 return True
         return False
+    
+    def get_user(self,usercpf):
+        for user in UsuariosManagement.users:
+            if user.cpf == usercpf:
+                return user
 
     def create_user(self,nome, usercpf, nascimento):
-        if UsuariosManagement.user_existe(usercpf):
+        if UsuariosManagement.user_existe(self, usercpf):
             print('não é possivel criar esse usuario, ele ja existe')
             return
         User = Usuario(nome=nome, cpf=usercpf, nascimento=nascimento)
         UsuariosManagement.users.append(User)
+
+
 
     
