ec650621ae117821fb625c37c837bd55a03cbca4
Gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-10-31 17:33:13 -0300

    instanciando

--- OO/PeooCiro/banco/classe_conta.py
+++ OO/PeooCiro/banco/classe_conta.py
@@ -1,34 +1,38 @@
-class Banco:
+from random import *
+class Conta:
     def __init__(self, numero_conta: str, agencia:str, dono:str, saldo:float):
         self.numero_conta = numero_conta
         self.agencia = agencia
         self.dono = dono
         self.saldo = saldo
-        listaContas = BancoManagement()
     
 class Usuario:
     def __init__(self, nome:str, cpf:str, nascimento:str):
         self.nome = nome
         self.cpf = cpf
         self.nascimento = nascimento
-        Usuarios = UsuariosManagement()
 
 class BancoManagement:
     banco_contas = []
 
     def show_contas(self):
-        for i in range(len(self.banco_contas)):
-            print(self.banco_contas[i])
+        for i in range(len(BancoManagement.banco_contas)):
+            print(BancoManagement.banco_contas[i]) 
 
-    def add_conta(self, conta):
-        if self.conta_existe(conta):
-            print('Não é possivel adicionar essa conta, ela ja existe no banco')
-            return
-        self.banco_contas.append(conta)
+    def create_conta(self, usercpf:str):
+        if UsuariosManagement.user_existe(self, usercpf):
+            conta_user = Conta(numero_conta=random.randint(1000,10000), agencia=str(input('Em qual agencia deseja criar? ')), dono= user, saldo=0)
+            if BancoManagement.conta_existe(conta_user):
+                print('Não é possivel adicionar essa conta, ela ja existe no banco')
+                return
+            BancoManagement.banco_contas.append(conta_user)
+            return conta_user
+        print('cpf nao existe, impossivel criar uma conta ')
+        return
         
 
     def conta_existe(self, conta):
-        for contas in self.banco_contas:
+        for contas in BancoManagement.banco_contas:
             if contas.numero_conta == conta.numero_conta and contas.agencia == conta.agencia:
                 return True
         return False
@@ -37,19 +41,20 @@ class UsuariosManagement:
     users = []
 
     def show_users(self):
-        for i in range(len(self.users)):
-            print(self.users[i])
+        for i in range(len(UsuariosManagement.users)):
+            print(UsuariosManagement.users[i])
 
-    def user_existe(self, user):
-        for users in self.users:
-            if users.cpf == user.cpf:
+    def user_existe(self, usercpf):
+        for users in UsuariosManagement.users:
+            if users.cpf == usercpf:
                 return True
         return False
 
-    def add_user(self, user):
-        if self.user_existe(user):
+    def create_user(self,nome, usercpf, nascimento):
+        if UsuariosManagement.user_existe(usercpf):
             print('não é possivel criar esse usuario, ele ja existe')
             return
-        self.users.append(user)
+        User = Usuario(nome=nome, cpf=usercpf, nascimento=nascimento)
+        UsuariosManagement.users.append(User)
 
     
--- OO/PeooCiro/banco/instancian_contas.py
+++ OO/PeooCiro/banco/instancian_contas.py
@@ -1,2 +1,12 @@
 from classe_conta import *
+banco = BancoManagement()
+usuarios = UsuariosManagement()
+
+def criar_usuario():
+    global usuarios
+    nome = str(input('Digite seu nome '))
+    cpf = str(input('Informe seu cpf '))
+    ano_nascimento = int(input('Ano de nascimento '))
+    usuarios.create_user(nome, cpf, ano_nascimento)
+
 
