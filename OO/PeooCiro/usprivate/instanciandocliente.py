1d583835518a59fdd9ed05edbc3c4180e590f5e7
gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-11-17 23:16:49 -0300

    instanciando cliente

--- OO/PeooCiro/usprivate/instanciandocliente.py
+++ OO/PeooCiro/usprivate/instanciandocliente.py
@@ -1,3 +1,7 @@
 from clie import  *
 
-cliente1 = Cliente()
+cliente1 = Cliente('gerson', 'luis gomes', '124.333.444-54', 21)
+if cliente1.verificaCPF(cliente1.getCPF()):
+    print('cliente valido')
+else:
+    print('cliente invalido')
